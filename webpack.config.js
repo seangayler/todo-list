3d3a0608b8fd1c6a2f4e582abb2e34bc40bc4733
Sean Gayler <seangayler@Seans-MacBook-Pro.local>
2020-02-16 17:30:56 +1000

    Configured basic loaders.

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,22 @@ module.exports = {
   output: {
     path: __dirname + '/dist',
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.css$/,
+        loader: 'style-loader!css-loader',
+        options: {
+          presets: ['css-loader', 'style-loader']
+        }
+      },
+      { test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      },
+    ]
   }
 }
